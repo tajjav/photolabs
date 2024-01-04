c7dd5e391988e797cd39102d81e36af9e0014ff6
tajjav <taj.jav@hotmail.com>
2024-01-04 16:42:01 -0700

    add PhotoListItem.jsx react code without styling

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,21 +1,19 @@
 import React from "react";
-
 import "../styles/PhotoListItem.scss";
 
 
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
 
-const PhotoListItem = () => {
-  /* Insert React */
+
+const PhotoListItem = (props) => {
+  
+  return (
+    <div>
+      <img src={props.imageSource} />
+      <img src={props.profile} />
+      <h2>{props.username}</h2>
+      <h3>{props.city}, {props.country}</h3>
+    </div>
+  );
 };
 
 export default PhotoListItem;
