3536ea09e8e195b1ba567dcc1f634101e1c78a61
tajjav <taj.jav@hotmail.com>
2024-01-10 10:22:28 -0700

    add heart icon with select and deselect

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -4,10 +4,24 @@ import FavIcon from './FavIcon';
 import '../styles/PhotoFavButton.scss';
 
 function PhotoFavButton() {
+  const [heartToggle, setHeartToggle] = useState(false);
+  
+  const handleClick = () => {
+    if(heartToggle === false) {
+      setHeartToggle(true);
+      
+    }
+    if(heartToggle === true) {
+      setHeartToggle(false);
+
+    }  
+  }
   return (
-    <div className="photo-list__fav-icon">
+    <div className="photo-list__fav-icon" onClick={handleClick}>
       <div className="photo-list__fav-icon-svg">
-        {/* Insert React */}
+        {/* insert React */}
+        {heartToggle === false && <FavIcon/>}
+        {heartToggle === true && <FavIcon selected={true} />}
       </div>
     </div>
   );
--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "../styles/PhotoListItem.scss";
+import PhotoFavButton from "./PhotoFavButton";
 
 
 
@@ -8,13 +9,14 @@ const PhotoListItem = (props) => {
   
   return (
     <div className="photo-list__item" >
+      <PhotoFavButton/>
       <img className="photo-list__image" src={props.imageSource} />
       <div className="photo-list__user-details" >
         <img className="photo-list__user-profile " src={props.profile} />
         <div className="photo-list__user-info">
           {props.username}
           <div className="photo-list__user-location">
-            {props.city}, {props.country}
+            {props.location.city}, {props.location.country}
           </div>
         </div>
       </div>
