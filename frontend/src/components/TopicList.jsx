e0f0aff2fb642dd9528b0eb287569f3829d45c3d
tajjav <taj.jav@hotmail.com>
2024-01-11 09:05:40 -0700

    PhotoList.jsx and TopicList.jsx set to use mock data photos.js and topics.js

--- frontend/src/components/TopicList.jsx
+++ frontend/src/components/TopicList.jsx
@@ -2,28 +2,29 @@ import React from "react";
 
 import "../styles/TopicList.scss";
 import TopicListItem from "./TopicListItem";
+import topics from "mocks/topics";
 
-const sampleDataForTopicList = [
-  {
-    id: "1",
-    slug: "topic-1",
-    title: "Nature",
-  },
-  {
-    id: "2",
-    slug: "topic-2",
-    title: "Travel",
-  },
-  {
-    id: "3",
-    slug: "topic-3",
-    title: "People",
-  },
-];
+// const sampleDataForTopicList = [
+//   {
+//     id: "1",
+//     slug: "topic-1",
+//     title: "Nature",
+//   },
+//   {
+//     id: "2",
+//     slug: "topic-2",
+//     title: "Travel",
+//   },
+//   {
+//     id: "3",
+//     slug: "topic-3",
+//     title: "People",
+//   },
+// ];
 
 const TopicList = () => {
 
-  const parsedTopicList = sampleDataForTopicList.map((item,index)=>{
+  const parsedTopicList = topics.map((item,index)=>{
     return <TopicListItem key={item.id} {...item} />
   })
   return (
